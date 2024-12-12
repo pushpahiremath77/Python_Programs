498e707964ce1b58dd9fb2f4f6ae0c134281b486
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-12 22:21:09 +0530

    Updated program using all() function

--- Day2/even_digit_num.py
+++ Day2/even_digit_num.py
@@ -1,10 +1,23 @@
-for i in range(100, 401):
-    temp = i 
-    while temp > 0:
-        digit = temp % 10  
-        if digit % 2 != 0:  
-            break  
-        temp = temp // 10  
-    else:
-        print(i, end=" ")
+# for i in range(100, 401):
+#     temp = i 
+#     while temp > 0:
+#         digit = temp % 10  
+#         if digit % 2 != 0:  
+#             break  
+#         temp = temp // 10  
+#     else:
+#         print(i, end=" ")
 
+
+def find_even_digit_numbers():
+    result = []
+    for num in range(100, 401):  
+        num_str = str(num)  
+        
+        if all(int(digit) % 2 == 0 for digit in num_str):
+            result.append(num_str)  
+    
+    print(",".join(result))  
+
+
+find_even_digit_numbers()
