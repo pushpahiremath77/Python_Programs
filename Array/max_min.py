5b9b149d50d41e486fdf81a137d7fa09fafec10c
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-14 21:51:49 +0530

    To get max & min from Array

---
+++ Array/max_min.py
@@ -0,0 +1,9 @@
+import array
+
+arr = array.array('i', [10, 20, 30, 40, 50])
+
+max_value = max(arr)
+min_value = min(arr)
+
+print(f"Maximum value: {max_value}")
+print(f"Minimum value: {min_value}")
