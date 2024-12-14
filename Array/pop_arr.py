2bfc7f5115c4e72734d890cb640ad551484e9888
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-14 15:43:21 +0530

    Array program using Pop method

---
+++ Array/pop_arr.py
@@ -0,0 +1,10 @@
+import array
+
+arr = array.array('i', [10, 20, 30, 40, 50])
+
+popped_element = arr.pop(2)
+
+print(f"Popped element: {popped_element}")
+print("Array after pop:")
+for item in arr:
+    print(item)
