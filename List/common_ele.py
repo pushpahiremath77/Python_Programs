fc4c38e78df73d3db7dd2c95de62925e06ba310a
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-16 01:23:15 +0530

    List operation programs

--- List/common_ele.py
+++ List/common_ele.py
@@ -1,12 +1,12 @@
 def common_element(list1, list2):
-    for item in list1:
-        if item in list2:
-            return True
-    return False
+    # for item in list1:
+    #     if item in list2:
+    #         return True
+    # return False
+
+    return list(set(list1) & set(list2))
 
 list1 = [1, 2, 3, 4, 5]
-list2 = [6, 7, 3, 9, 10]
+list2 = [6, 7, 3, 9, 4, 10]
 
-result = common_element(list1, list2)
-
-print("Lists have a common element? --->", result)
+print("Common elements:", common_element(list1, list2))
