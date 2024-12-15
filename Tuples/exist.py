ba6f9ad243ddac9cbf86588404be98287d4375ca
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-16 01:28:43 +0530

    To check the existence of an item

---
+++ Tuples/exist.py
@@ -0,0 +1,20 @@
+tuple = (1, 2, 3, 4, 5)
+element = 6
+if element in tuple:
+    print(f"{element} exists in the tuple.")
+else:
+    print(f"{element} does not exist in the tuple.")
+
+
+
+# my_tuple = (1, 2, 3, 4, 5)
+# element = 3
+# exists = False
+# for item in my_tuple:
+#     if item == element:
+#         exists = True
+#         break
+# if exists:
+#     print(f"{element} exists in the tuple.")
+# else:
+#     print(f"{element} does not exist in the tuple.")
