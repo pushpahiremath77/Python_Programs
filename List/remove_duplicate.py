3437ed5e3890f5550c05667573bd710a4009075e
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-16 01:24:28 +0530

    List operation programs

--- List/remove_duplicate.py
+++ List/remove_duplicate.py
@@ -4,3 +4,24 @@ def remove_duplicates(list1):
 
 list1 = [1, 2, 2, 3, 4, 4, 5]
 remove_duplicates(list1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def remove_duplicates(list1):
+#     return [list(x) for x in set(tuple(x) for x in list1)]
+
+# list1 = [[10, 20], [40], [30, 56, 25], [10, 20], [33], [40]]
+# print("New list without duplicates:", remove_duplicates(list1))
